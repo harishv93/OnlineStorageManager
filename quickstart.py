d131969ea06bc2a3eef3a9d4495666388c7f00ba
harish <harish@harish-Inspiron-N5010.(none)>
2013-01-30 16:33:58 +0530

    googledrive integration

---
+++ quickstart.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import httplib2
+import pprint
+
+from apiclient.discovery import build
+from apiclient.http import MediaFileUpload
+from oauth2client.client import OAuth2WebServerFlow
+
+CLIENT_ID = '988728566209.apps.googleusercontent.com'
+CLIENT_SECRET = 'CwblTNS_mS6gCmwyYLrJ6uOx'
+
+OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
+
+REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
+
+FILENAME = 'document.txt'
+
+flow = OAuth2WebServerFlow(CLIENT_ID,CLIENT_SECRET,OAUTH_SCOPE,REDIRECT_URI)
+authorize_url = flow.step1_get_authorize_url()
+print 'Go to the following link in your browser: ' + authorize_url
+code = raw_input('Enter Verification Code: ').strip()
+credentials = flow.step2_exchange(code)
+
+http = httplib2.Http()
+http = credentials.authorize(http)
+
+drive_service = build('drive','v2',http=http)
+media_body = MediaFileUpload(FILENAME,mimetype='text/plain',resumable=True)
+body = {
+    'title' : 'My document',
+    'description' : 'A test document',
+    'mimeType' : 'text/plain'
+}
+file = drive_service.files().insert(body=body,media_body=media_body).execute()
+pprint.pprint(file)
